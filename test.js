465b8fdc0da054eebd8ae946c8264354e4866d8d
robertkrimen <robertkrimen@gmail.com>
2011-10-06 13:44:01 -0700

    Add test.js

---
+++ test.js
@@ -0,0 +1,31 @@
+function Xyzzy() {
+    return "Nothing happens";
+    
+    // Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    // In sed massa id mauris ultrices dictum.
+    // Pellentesque posuere eros venenatis neque ornare feugiat.
+    // Quisque vitae nisi nec diam hendrerit vehicula.
+
+    // Quisque at risus eget dolor molestie elementum.
+    // Sed malesuada nulla vitae libero tristique mollis.
+    // In vestibulum pellentesque eros, vitae tristique ante commodo sit amet.
+    // Vivamus quis nibh eu augue suscipit laoreet et id libero.
+    // Nullam vel ipsum magna, at dapibus risus.
+
+    // Nunc pulvinar interdum orci, vitae venenatis urna cursus nec.
+    // Etiam interdum erat quis purus interdum porttitor dapibus sem vehicula.
+    // Proin placerat mauris fermentum ante venenatis aliquet nec vel justo.
+
+    // Aenean mattis odio id lectus aliquam dapibus.
+    //
+ 
+    var alpha, beta;
+
+    // Sed dictum erat eu neque sollicitudin blandit.
+    // Etiam ullamcorper quam sed metus dictum varius.
+    // Sed sit amet mi mi, vitae dignissim neque.
+    // Praesent facilisis volutpat magna, ac lacinia nulla tincidunt id.
+    // Vestibulum in dolor eu lectus tincidunt ullamcorper.
+    // Maecenas convallis tempus massa, non tincidunt purus mattis id.
+
+}
